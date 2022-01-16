482be0353ae8e62a9c7421fe950e389351dab919
rohilpinto <rohilpinto@users.noreply.github.com>
2022-01-16 08:45:19 +0530

    fix1

--- src/components/AudioProfileCard.js
+++ src/components/AudioProfileCard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Card, Divider, Typography, CardMedia, IconButton, Tooltip, Fade } from "@mui/material";
+import { Box, Card, Divider, Typography, IconButton, Tooltip, Fade } from "@mui/material";
 
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import audioGif from "../assets/audio_play.gif";
@@ -32,51 +32,51 @@ const AudioProfileCard = ({ name = "yourname", profilePic = "https://cdn.pixabay
   }, []);
 
   return (
-    <Fade in={true}>
-      <Card sx={{ bgcolor: "", width: "310px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", p: 2, gap: 2 }}>
-        <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <Box component="img" src={profilePic} alt="profile" sx={{ borderRadius: "100px", width: "120px" }} />
-        </Box>
-        <Box sx={{ width: "80%", textAlign: "center", "p, h1,h2,h3,h4,h5,h6": { color: "#263238" } }}>
-          {/* name */}
-          <Typography variant="h4" component="h1" sx={{ fontSize: "24px" }}>
-            {name}
+    // <Fade in={true}>
+    <Card sx={{ bgcolor: "", width: "310px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", p: 2, gap: 2 }}>
+      <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Box component="img" src={profilePic} alt="profile" sx={{ borderRadius: "100px", width: "120px" }} />
+      </Box>
+      <Box sx={{ width: "80%", textAlign: "center", "p, h1,h2,h3,h4,h5,h6": { color: "#263238" } }}>
+        {/* name */}
+        <Typography variant="h4" component="h1" sx={{ fontSize: "24px" }}>
+          {name}
+        </Typography>
+        <Tooltip title="Phonetic Name" sx={{ bgcolor: "black" }} arrow>
+          <Typography variant="h5" color={"#90a4ae !important"} sx={{ fontSize: "18px", mt: 1 }}>
+            ({phoneticName})
           </Typography>
-          <Tooltip title="Phonetic Name" sx={{ bgcolor: "black" }} arrow>
-            <Typography variant="h5" color={"#90a4ae !important"} sx={{ fontSize: "18px", mt: 1 }}>
-              ({phoneticName})
-            </Typography>
-          </Tooltip>
-          <Divider sx={{ width: "100px", margin: "auto", mt: 1 }} />
+        </Tooltip>
+        <Divider sx={{ width: "100px", margin: "auto", mt: 1 }} />
 
-          <Typography variant="body1" sx={{ mt: 1 }} color={"#90a4ae !important"} fontWeight={"500"}>
-            {pronoun}
-          </Typography>
+        <Typography variant="body1" sx={{ mt: 1 }} color={"#90a4ae !important"} fontWeight={"500"}>
+          {pronoun}
+        </Typography>
 
-          {/* audio */}
-        </Box>
-        <Box>
-          <Tooltip open={toolTip} onClose={() => setToolTip(false)} title="Click to hear name" sx={{ bgcolor: "black" }} arrow>
-            <IconButton
-              sx={{ bgcolor: "#e16643", color: "#fff", "&:hover": { bgcolor: "#ff987b", color: "grey" } }}
-              onClick={() => {
-                handlePlay();
-                setToolTip(false);
-              }}
-            >
-              {!playing ? <PlayArrowIcon sx={{ height: 38, width: 38 }} /> : <PauseIcon sx={{ height: 38, width: 38 }} />}
-            </IconButton>
-          </Tooltip>
-        </Box>
-        {/* animation on audio play */}
+        {/* audio */}
+      </Box>
+      <Box>
+        <Tooltip open={toolTip} onClose={() => setToolTip(false)} title="Click to hear name" sx={{ bgcolor: "black" }} arrow>
+          <IconButton
+            sx={{ bgcolor: "#e16643", color: "#fff", "&:hover": { bgcolor: "#ff987b", color: "grey" } }}
+            onClick={() => {
+              handlePlay();
+              setToolTip(false);
+            }}
+          >
+            {!playing ? <PlayArrowIcon sx={{ height: 38, width: 38 }} /> : <PauseIcon sx={{ height: 38, width: 38 }} />}
+          </IconButton>
+        </Tooltip>
+      </Box>
+      {/* animation on audio play */}
 
-        {playing ? (
-          <Fade easing={{ enter: "0.9", exit: "0.9" }} in={playing} sx={{ mt: 1 }}>
-            <img src={audioGif} alt="" />
-          </Fade>
-        ) : null}
-      </Card>
-    </Fade>
+      {playing ? (
+        <Fade easing={{ enter: "0.9", exit: "0.9" }} in={playing} sx={{ mt: 1 }}>
+          <img src={audioGif} alt="" />
+        </Fade>
+      ) : null}
+    </Card>
+    // </Fade>
   );
 };
 
